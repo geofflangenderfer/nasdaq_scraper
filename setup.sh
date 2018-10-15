8880c3af06ede2b40c1c47cc0eb14cd2b7b190c9
Robert He <roberthe@Roberts-MacBook-Air-2.local>
2018-10-15 15:15:45 -0700

    fixed setup

--- setup.sh
+++ setup.sh
@@ -5,8 +5,27 @@ if [ -d "$X" ]; then
     rm -rf ~/nasdaq_scraper
 fi
 
+# install homebrew if not present
+if [ ! "$(command -v brew)" ]; then
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        export PATH=/usr/local/bin:/usr/local/sbin:$PATH
+fi
+
+# install git if not present
+if [ ! "$(command -v git)" ]; then
+        brew doctor
+        brew install git
+fi
+
+# install python if not present
+if [ ! "$(command -v python3)" ]; then
+        brew install python
+fi
+        
 git clone https://github.com/geofflangenderfer/nasdaq_scraper.git \
     ~/nasdaq_scraper
+
+pip3 install -r ~/nasdaq_scraper/requirements.txt
 
 Y=~/Desktop/nasdaq_scraper/
 if [ -d "$Y" ]; then
