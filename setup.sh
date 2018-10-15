a5a621fb4cd04ed37211ae696186b78eaf8a23b2
Robert He <roberthe@Roberts-MacBook-Air-2.local>
2018-10-15 09:46:17 -0700

    fixed filepath bug

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+X=~/nasdaq_scraper/
+if [ -d "$X" ]; then 
+    rm -rf ~/nasdaq_scraper
+fi
+
+git clone https://github.com/geofflangenderfer/nasdaq_scraper.git \
+    ~/nasdaq_scraper
+
+Y=~/Desktop/nasdaq_scraper/
+if [ -d "$Y" ]; then
+	rm -rf ~/Desktop/nasdaq_scraper
+fi
+
+mkdir ~/Desktop/nasdaq_scraper
+
+cp ~/nasdaq_scraper/nasdaq_scraper \
+   ~/nasdaq_scraper/EarningsWatchList.xlsx \
+   ~/Desktop/nasdaq_scraper
